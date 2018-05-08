b4156c3194558e7d1e323c61e9167a81825274c0
imjinbro <jinbro@likelion.org>
2018-05-08 14:19:31 +0900

    init project

---
+++ src/main/java/com/imjinbro/mypage/MyPageApplication.java
@@ -0,0 +1,12 @@
+package com.imjinbro.mypage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPageApplication.class, args);
+    }
+}
